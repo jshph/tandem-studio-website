817e805f076b775adf10fc1967e78030ae5e934f
Michael Yu <m.yu8088@gmail.com>
2017-12-01 17:27:29 -0600

    removed unused fonts, use cdns for external resources, sped up gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,42 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var pug = require('gulp-pug');
-var autoprefix = require('gulp-autoprefixer');
-var cssmin = require('gulp-cssmin');
-var filter = require('gulp-filter')
-var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var jsmin = require('gulp-jsmin');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const autoprefix = require('gulp-autoprefixer');
+const cssmin = require('gulp-cssmin');
+const filter = require('gulp-filter')
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const jsmin = require('gulp-jsmin');
+const clean = require('gulp-clean');
+const cache = require('gulp-cache');
 
-gulp.task('js', function () {
+gulp.task('js', function() {
     return gulp.src('./src/js/*.js')
         .pipe(jsmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('js-watch', ['js'], function (done) {
+gulp.task('js-watch', ['js'], function(done) {
     browserSync.reload();
     done();
 });
 
-gulp.task('scss', function () {
+gulp.task('scss', function() {
     return gulp.src('./src/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefix())
         .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('scss-watch', ['scss'], function (done) {
+gulp.task('scss-watch', ['scss'], function(done) {
     browserSync.reload();
     done();
 });
@@ -38,13 +44,13 @@ gulp.task('scss-watch', ['scss'], function (done) {
 gulp.task('templates', function() {
     return gulp.src('./src/templates/*.pug')
         .pipe(pug())
-        .pipe(filter(function (file) {
+        .pipe(filter(function(file) {
             return !/\/_/.test(file.path) && !/^_/.test(file.relative);
         }))
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('pug-watch', ['templates'], function (done) {
+gulp.task('pug-watch', ['templates'], function(done) {
     browserSync.reload();
     done();
 });
@@ -56,11 +62,18 @@ gulp.task('fonts', function() {
 
 gulp.task('imgs', function() {
     return gulp.src('./src/imgs/**/*')
-        .pipe(imagemin({verbose: true}))
+        .pipe(cache(imagemin({
+            verbose: true
+        })))
         .pipe(gulp.dest('./dist/imgs'));
 });
 
-gulp.task('watch', ['templates','scss','js'], function () {
+gulp.task('clean', function() {
+	return gulp.src('./dist', {read: false})
+		.pipe(clean());
+})
+
+gulp.task('watch', ['templates', 'scss', 'js'], function() {
 
     browserSync.init({
         server: {
@@ -70,8 +83,8 @@ gulp.task('watch', ['templates','scss','js'], function () {
 
     gulp.watch("./src/js/*.js", ['js-watch']);
     gulp.watch("./src/templates/**/*.pug", ['pug-watch']);
-    gulp.watch("./src/scss/**/*.scss", ['scss-watch'])
+    gulp.watch("./src/scss/**/*.scss", ['scss-watch']);
 });
 
 // only run 'imgs' task when you build, it takes forever
-gulp.task('build', ['templates','scss', 'js', 'imgs', 'fonts']);
+gulp.task('build', ['templates', 'scss', 'js', 'imgs', 'fonts']);
--- src/js/script.js
+++ src/js/script.js
@@ -3,38 +3,38 @@ var ticking = false;
 
 $(document).ready(function() {
 
-window.addEventListener('scroll', function(e) {
-  prev_scrollY = window.scrollY;
+  window.addEventListener('scroll', function(e) {
+    prev_scrollY = window.scrollY;
 
-  if (!ticking && prev_scrollY < 400) {
-    $('#header').removeClass('scrolled-to-limit');
-    $('#nav').removeClass('scrolled-to-limit');
+    if (!ticking && prev_scrollY < 400) {
+      $('#header').removeClass('scrolled-to-limit');
+      $('#nav').removeClass('scrolled-to-limit');
 
-    window.requestAnimationFrame(function() {
+      window.requestAnimationFrame(function() {
 
-      dim = 190 * (1 + scrollY / 580 * 50);
+        dim = 190 * (1 + scrollY / 580 * 50);
 
-      $('#logo-circle').css({
-        'height': dim,
-        'width': dim,
-        'border-radius': dim,
-        'left': String(-4 * (1 + scrollY)) + "%",
-        'top': String(12 - scrollY / 580 * 800) + "%"
-        //'opacity': Math.pow((580 - scrollY) / 580, 1)
-      })
+        $('#logo-circle').css({
+          'height': dim,
+          'width': dim,
+          'border-radius': dim,
+          'left': String(-4 * (1 + scrollY)) + "%",
+          'top': String(12 - scrollY / 580 * 800) + "%"
+          //'opacity': Math.pow((580 - scrollY) / 580, 1)
+        })
 
-      $('#hero table').css({
-        'margin-top': -scrollY * 2
-      })
+        $('#hero table').css({
+          'margin-top': -scrollY * 2
+        })
 
-      ticking = false;
-    });
-    ticking = true;
+        ticking = false;
+      });
+      ticking = true;
 
-  } else {
-    $('#header').addClass('scrolled-to-limit');
-    $('#nav').addClass('scrolled-to-limit');
-  }
-})
+    } else {
+      $('#header').addClass('scrolled-to-limit');
+      $('#nav').addClass('scrolled-to-limit');
+    }
+  });
 
 });
